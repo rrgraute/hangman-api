0f80993d0a1c6d776e4abeccd2ed6b6c57cd7c6e
Ryan Graute <rrgraute@gmail.com>
2015-12-18 20:48:04 +0100

    Created a bundle && word entity created

---
+++ src/HangmanBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HangmanBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('HangmanBundle:Default:index.html.twig');
+    }
+}
---
+++ src/HangmanBundle/Entity/word.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace HangmanBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * word
+ *
+ * @ORM\Table(name="word")
+ * @ORM\Entity(repositoryClass="HangmanBundle\Repository\wordRepository")
+ */
+class word
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="word", type="string", length=11, unique=true)
+     */
+    private $word;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set word
+     *
+     * @param string $word
+     *
+     * @return word
+     */
+    public function setWord($word)
+    {
+        $this->word = $word;
+
+        return $this;
+    }
+
+    /**
+     * Get word
+     *
+     * @return string
+     */
+    public function getWord()
+    {
+        return $this->word;
+    }
+}
+
